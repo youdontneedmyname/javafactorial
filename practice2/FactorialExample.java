9e52f29808f2b753f38b6ba5d13b8b8fdc5af1ea
youdontneedmyname <118007675+youdontneedmyname@users.noreply.github.com>
2023-07-14 14:03:35 +0400

    Add files via upload

---
+++ practice2/FactorialExample.java
@@ -0,0 +1,17 @@
+package practice2;
+
+public class FactorialExample {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 3;
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
